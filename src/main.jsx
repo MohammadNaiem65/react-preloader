2e584b8c6c7cc0bc3b6210588e1c431cffe76da1
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-01-17 09:33:55 +0600

    add: created pre-loader for my portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import gsap from 'gsap';
 import App from './App.jsx';
 import './index.css';
 
-window.gsap = gsap;
-
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+setTimeout(() => {
+    createRoot(document.getElementById('root')).render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+}, 10000);
